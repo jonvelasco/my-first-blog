d93aab31f6e1b2cf9628bf21420d962978259a2c
Jonathan Velasco <33075264+jonvelasco@users.noreply.github.com>
2017-11-04 16:55:41 -0200

    Mudança em settings.py para colocar o site no ar
    Vi isso quando upei o site para PythonAnywhere:
    "You're seeing this error because you have DEBUG = True in your Django settings file. Change that to False, and Django will display a standard page generated by the handler for this status code. "
    Então, mudei o valor de DEBUG para False

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '66%z_bu8qohl+0dl7_oe97&o9q29hb%4@$f_#b%w^m)q_=!qq1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
 
